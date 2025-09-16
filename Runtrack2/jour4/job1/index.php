eb39a9b64cc7f7b7dafedd95e6ee20082d71d4ec
antoine-leca <antoine.leca@laplateforme.io>
2025-09-16 13:08:04 +0200

    ajout du jour4-job1

---
+++ Runtrack2/jour4/job1/index.php
@@ -0,0 +1,34 @@
+<?php
+    $numberOfArguments = 0;
+    foreach ($_GET as $key => $value) {
+        $numberOfArguments++;
+    }
+    echo "Il y a $numberOfArguments arguments dans l'URL.<br>";
+
+    if (isset($_GET['name']) && isset($_GET['mail'])) {
+        $name = $_GET['name'];
+        $mail = $_GET['mail'];
+    }
+    echo "<pre>";
+    var_dump($_GET);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="GET">
+        <label for="name">Nom : </label>
+        <input type="text" name="name" id="name">
+
+        <label for="mail">Mail : </label>
+        <input type="mail" name="mail" id="mail">
+
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
