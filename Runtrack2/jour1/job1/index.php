117257ccee20a2c89b15b93346f2700a40297dc7
antoine-leca <antoine.leca@laplateforme.io>
2025-09-16 10:47:46 +0200

    ajout de la médiathèque dans le repo + reuntrack2-jour1-job1

---
+++ Runtrack2/jour1/job1/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    //string
+    $str = "LaPlateforme";
+    echo $str . "<br>";
+
+    $str2 = "Vive";
+    $str3 = "!";
+    echo $str2 . " " . $str . " " . $str3 . "<br>";
+
+    // int
+    $val = 6;
+    echo $val . "<br>";
+
+    $val = $val + 4;
+    echo $val . "<br>";
+
+    // booléen
+    $myBool = true;
+    echo $myBool . "<br>";
+
+    $myBool = false;
+    echo $myBool . "<br>"; 
+?>
