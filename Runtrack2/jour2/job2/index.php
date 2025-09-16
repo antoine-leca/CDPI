fdbd2580cf7b84609bec8588907fc984a5e10f26
antoine-leca <antoine.leca@laplateforme.io>
2025-09-16 11:11:26 +0200

    ajout du jour2-job2

---
+++ Runtrack2/jour2/job2/index.php
@@ -0,0 +1,21 @@
+<?php
+    $val = 0;
+
+    for ($i=0; $i <= 1337 ; $i++) { 
+        switch ($i) {
+            case 26 :
+                break;
+            case 37 :
+                break;
+            case 88 :
+                break;
+            case 1111 :
+                break;
+            case 3233 :
+                break;
+            default:
+                echo $i . "<br>";
+                break;
+        }
+    }
+?>
